1f814c7ecc264ef85c2c7816bf7216aa1f9a2724
Chris Shaw <103132499+Chris-I-Shaw@users.noreply.github.com>
2022-07-15 14:04:53 -0700

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,32 @@
+namespace MonsterSlayer;
+
+abstract class Enemy : Character
+{
+    // The enum used in enemies
+    public enum Realm
+    {
+        EARTH, OUTWORLD, UNDERWORLD, VULCAN
+    }
+
+    public Realm EnemyRealm { get; init; }
+
+    public Enemy(int healthLevel, int power, String attackMessage, int level, Realm realm) : base(healthLevel, power, attackMessage)
+    {
+        // TODO: set the Level
+        this.Level = level;
+
+        // TODO: assign realm to this EnemyRealm
+        this.EnemyRealm = realm;
+
+    }
+
+    // This method determines if Enemy can fight another character.
+    // Enemy can fight another character ONLY if its level is less than or equal to this level
+    // and c is an instance of Warrior class (the other character is a warrior)
+    public override bool CanFight(Character c)
+    {
+        // TODO: change the return statement to return true if level of c is <= to the level of this enemy AND 
+        //  the type of c is Warrior
+        return(c.Level <= Level && c is Warrior);
+    }
+}
